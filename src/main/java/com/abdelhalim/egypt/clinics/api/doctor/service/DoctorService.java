ccf7c519f82cfc1ce7deaad3b4dbbb4c67b95ac3
abdomi7 <56141588+abdomi7@users.noreply.github.com>
2023-04-04 01:41:07 +0200

    handling image in DoctorService

--- src/main/java/com/abdelhalim/egypt/clinics/api/doctor/service/DoctorService.java
+++ src/main/java/com/abdelhalim/egypt/clinics/api/doctor/service/DoctorService.java
@@ -7,6 +7,8 @@ import com.abdelhalim.egypt.clinics.api.doctor.mapper.DoctorDtoMapper;
 import com.abdelhalim.egypt.clinics.api.doctor.repository.DoctorRepository;
 import com.abdelhalim.egypt.clinics.api.specialty.entity.Specialty;
 import com.abdelhalim.egypt.clinics.api.specialty.repository.SpecialtyRepository;
+import com.abdelhalim.egypt.clinics.utils.Base64Utils;
+import com.abdelhalim.egypt.clinics.utils.ImageUtils;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +17,9 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 @Slf4j
@@ -33,13 +37,23 @@ public class DoctorService {
         Doctor entity = new Doctor();
         entity.setName(doctorDtoWithSpecialityId.getName());
         entity.setNameAr(doctorDtoWithSpecialityId.getNameAr());
-        entity.setImage(doctorDtoWithSpecialityId.getImage());
         List<Specialty> specialtyList = new ArrayList<>(specialtyRepository.findAllById(doctorDtoWithSpecialityId.getSpecialityIds()));
         entity.setSpecialtyList(specialtyList);
-        repository.save(entity);
+
+        try {
+            String extension = Base64Utils.getFileExtensionFromBase64(doctorDtoWithSpecialityId.getImage());
+            byte[] decodedBytes = Base64.getDecoder().decode(doctorDtoWithSpecialityId.getImage().split(",")[1]);
+
+            String url = ImageUtils.uploadObjectFromMemory("clink-3b1fe.appspot.com", "doctor/" + entity.getId(), decodedBytes, extension);
+            entity.setImage(url);
+            repository.save(entity);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void deleteById(Long id) {
+        ImageUtils.deleteImage("clink-3b1fe.appspot.com", "doctor/" + id);
         repository.deleteById(id);
     }
 
@@ -59,9 +73,18 @@ public class DoctorService {
         Doctor doctor1 = repository.getReferenceById(id);
         doctor1.setName(doctor.getName());
         doctor1.setNameAr(doctor.getNameAr());
-        doctor1.setImage(doctor.getImage());
         List<Specialty> specialtyList = new ArrayList<>(specialtyRepository.findAllById(doctor.getSpecialityIds()));
         doctor1.setSpecialtyList(specialtyList);
-        repository.save(doctor1);
+        try {
+            ImageUtils.deleteImage("clink-3b1fe.appspot.com", "doctor/" + id);
+            String extension = Base64Utils.getFileExtensionFromBase64(doctor.getImage());
+            byte[] decodedBytes = Base64.getDecoder().decode(doctor.getImage().split(",")[1]);
+
+            String url = ImageUtils.uploadObjectFromMemory("clink-3b1fe.appspot.com", "specialty/" + id, decodedBytes, extension);
+            doctor1.setImage(url);
+            repository.save(doctor1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
