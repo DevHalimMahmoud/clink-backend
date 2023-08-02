79676194cb361bdb1081d4da027f732c60191034
abdomi7 <56141588+abdomi7@users.noreply.github.com>
2023-08-03 00:25:11 +0300

    global exception handling improvement

--- src/main/java/com/abdelhalim/egypt/clinics/config/GlobalExceptionHandler.java
+++ src/main/java/com/abdelhalim/egypt/clinics/config/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.abdelhalim.egypt.clinics.config;
 
 import com.abdelhalim.egypt.clinics.base.BaseResponse;
+import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,18 +12,36 @@ import org.springframework.web.bind.annotation.RestController;
 @ControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
 
-
-    @ExceptionHandler
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<BaseResponse<Object>> handleException(Exception exc) {
-
         BaseResponse<Object> error = BaseResponse
                 .builder()
                 .message(exc.getMessage())
                 .status(HttpStatus.BAD_REQUEST.value())
                 .timeStamp(System.currentTimeMillis()).build();
 
-
-        // return ResponseEntity
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<BaseResponse<Object>> handleValidationException(ValidationException exc) {
+        BaseResponse<Object> error = BaseResponse
+                .builder()
+                .message(exc.getMessage())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .timeStamp(System.currentTimeMillis()).build();
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<BaseResponse<Object>> handleGenericException(Throwable exc) {
+        BaseResponse<Object> error = BaseResponse
+                .builder()
+                .message(exc.getMessage())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .timeStamp(System.currentTimeMillis()).build();
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
