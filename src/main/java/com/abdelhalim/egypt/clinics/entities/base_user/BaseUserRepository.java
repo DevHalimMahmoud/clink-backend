ceafa69ca06ac419677c1244371959ae4db39884
abdomi7 <56141588+abdomi7@users.noreply.github.com>
2023-08-07 21:43:52 +0300

    adding BaseUserRepository

---
+++ src/main/java/com/abdelhalim/egypt/clinics/entities/base_user/BaseUserRepository.java
@@ -0,0 +1,15 @@
+package com.abdelhalim.egypt.clinics.entities.base_user;
+
+import com.abdelhalim.egypt.clinics.entities.patient.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BaseUserRepository extends JpaRepository<BaseUser, Long>, JpaSpecificationExecutor<BaseUser> {
+
+    Optional<BaseUser> findByPhone(String phone);
+    Optional<BaseUser> findByEmail(String email);
+}
